2254c85e502e2b401ba382561d4cd009a0ee4e35
eminem54 <eminem54@naver.com>
2018-07-13 14:06:51 +0900

    gg

--- src/algospot/Picnic.py
+++ src/algospot/Picnic.py
@@ -49,7 +49,7 @@ def findUntaken(isTaken, lastIdx):
 
 
 user_input = '''
-3
+3   
 2 1
 0 1
 4 6
--- src/algospot/josephus.py
+++ src/algospot/josephus.py
@@ -2,45 +2,32 @@
 def josep(N, K):
     i = 0
     Kindex = -1
+    length = len(people)
     j=0
-    while 1: #죽이는 과정
+    while length > 2: #죽이는 과정
         j += 1
         print(j)
-        if isdied[i] == True:
-            i += 1
-            if i == N:
-                i = 0
-            continue
-
         Kindex += 1
-        if Kindex % K == 0:  # 누구를 죽일까
-            isdied[i] = True  # i번째 죽음]
+        if Kindex % K == 0:  # 누구를 죽일까  # i번째 죽이고 리스트에서 삭제
+            people.pop(i)
+            length -= 1
+            i -= 1
 
         i += 1
-        if i == N:
+        if i >= length:
             i = 0
-
-        if isdied.count(False) == 2:
-            break;
 
 
 repeat = int(input())
-
+people = []
 for x in range(repeat):
     line = input().split(' ')
-    isdied = []
     people = []
     for i in range(1, int(line[0])+1):
         people.append(i)
-        isdied.append(False)
     josep(int(line[0]), int(line[1]))
 
-    output = []
-    for i in range(int(line[0])):
-        if isdied[i] == False:
-            output.append(people[i])
-
-    print(output[0], output[1])
+    print(people[0], people[1])
 
 
 
